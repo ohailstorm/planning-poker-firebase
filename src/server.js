db45bfc7b7b0225980058088b0455ad6fddec407
ohailstorm <ohailstorm@users.noreply.github.com>
2020-03-04 13:52:14 +0100

    dockerize app

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import compression from 'compression';
-import ssr from './routes/ssr';
-import api from './routes/firebaseApi';
+import express from "express";
+import compression from "compression";
+import ssr from "./routes/ssr";
+import api from "./routes/firebaseApi";
 
 const app = express();
 
 app.use(compression());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json()); // for parsing application/json
 
-app.use('/api', api);
-app.use('/', ssr);
+app.use("/api", api);
+app.use("/", ssr);
 
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 8080;
 app.listen(port, function listenHandler() {
   console.info(`Running on ${port}...`);
 });
