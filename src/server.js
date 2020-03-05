d725c23a33807ab154ab0f5be0162ca43e486b5a
ohailstorm <ohailstorm@users.noreply.github.com>
2020-03-05 10:46:57 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,16 +9,18 @@ app.use(compression());
 app.use(express.static("public"));
 app.use(express.json()); // for parsing application/json
 
+app.use(function(req, res, next) {
+  console.log(req.host);
+
+  if (!req.secure && req.host !== "localhost") {
+    res.redirect("https://" + req.headers.host + req.url);
+  } else next();
+});
+
 app.use("/api", api);
 app.use("/", ssr);
 
 const port = process.env.PORT || 8080;
-
-app.use(function(req, res) {
-  if (req.protocol === "http") {
-    res.redirect("https://" + req.headers.host + req.url);
-  } else next();
-});
 
 app.listen(port, function listenHandler() {
   console.info(`Running on ${port}...`);
